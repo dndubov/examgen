8917aa88f8a45ed002efc3bcfaa5863410125073
Dmitriy <dmitriyndubov@gmail.com>
2025-07-02 15:09:51 +0500

    initial commit

---
+++ src/main/java/org/skypro/examgen/ExamgenApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.examgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamgenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/examgen/ExamgenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.examgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
